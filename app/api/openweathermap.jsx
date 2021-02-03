85ccf8f0c90f25cb72696463593751acac5f6c42
Christopher Bishop <cbishop@solutionstream.com>
2021-02-02 17:01:11 -0700

    getting data from openweathermap via axios

---
+++ app/api/openweathermap.jsx
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=618a99d58a8b391a376ef599dc0e28a2';
+
+// 618a99d58a8b391a376ef599dc0e28a2
+
+module.exports = {
+  getTemp: function(location){
+    var encodedLocation = encodeURIComponent(location)
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function(res){
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function(res){
+      throw new Error(res.data.message);
+    });
+  }
+}
